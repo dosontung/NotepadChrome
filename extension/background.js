823b703ed687e7976464743e2180f6386885acb8
Đỗ Sơn Tùng - ITC - AB - Software Engineer <tung.do2@mservice.com.vn>
2023-02-21 16:04:22 +0700

    update append function

--- extension/background.js
+++ extension/background.js
@@ -24,41 +24,48 @@ chrome.runtime.onInstalled.addListener(function () {
 
 // Open a new search tab when the user clicks a context menu
 chrome.contextMenus.onClicked.addListener((item, tab) => {
-  const tld = item.menuItemId
-  let url = new URL(`https://google.${tld}/search`)
-  url.searchParams.set('q', item.selectionText)
-  chrome.tabs.create({ url: url.href, index: tab.index + 1 });
+  const item_text = item.selectionText;
+  chrome.storage.local.get('notepad', function (data) {
+    console.log('notepad data', data);
+    let notepad_text = data.notepad;
+    if (typeof notepad_text === "undefined") {
+        notepad_text = "";
+    }
+    chrome.storage.local.set({notepad: notepad_text + '<div>' + item_text + "</div>"}, function () {
+          console.log("Notepad updated with " +  item.selectionText);
+    });
+  });
 });
 
-// Add or removes the locale from context menu
-// when the user checks or unchecks the locale in the popup
-chrome.storage.onChanged.addListener(({ enabledTlds }) => {
-  if (typeof enabledTlds === 'undefined') return
-
-  let allTlds = Object.keys(tldLocales)
-  let currentTlds = new Set(enabledTlds.newValue);
-  let oldTlds = new Set(enabledTlds.oldValue ?? allTlds);
-  let changes = allTlds.map((tld) => ({
-    tld,
-    added: currentTlds.has(tld) && !oldTlds.has(tld),
-    removed: !currentTlds.has(tld) && oldTlds.has(tld)
-  }))
-
-  for (let { tld, added, removed } of changes) {
-    if (added) {
-      chrome.contextMenus.create({
-        id: tld,
-        title: tldLocales[tld],
-        type: 'normal',
-        contexts: ['selection'],
-      });
-    }
-    else if (removed) {
-      chrome.contextMenus.remove(tld);
-    }
-  }
-
-});
+////// Add or removes the locale from context menu
+//// when the user checks or unchecks the locale in the popup
+//chrome.storage.onChanged.addListener(({ enabledTlds }) => {
+//  if (typeof enabledTlds === 'undefined') return
+//
+//  let allTlds = Object.keys(tldLocales)
+//  let currentTlds = new Set(enabledTlds.newValue);
+//  let oldTlds = new Set(enabledTlds.oldValue ?? allTlds);
+//  let changes = allTlds.map((tld) => ({
+//    tld,
+//    added: currentTlds.has(tld) && !oldTlds.has(tld),
+//    removed: !currentTlds.has(tld) && oldTlds.has(tld)
+//  }))
+//
+//  for (let { tld, added, removed } of changes) {
+//    if (added) {
+//      chrome.contextMenus.create({
+//        id: tld,
+//        title: tldLocales[tld],
+//        type: 'normal',
+//        contexts: ['selection'],
+//      });
+//    }
+//    else if (removed) {
+//      chrome.contextMenus.remove(tld);
+//    }
+//  }
+//
+//});
 
 
 
